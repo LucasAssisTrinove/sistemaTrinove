2006ea4453bc03370b827474f0cee582939e8fdb
Lucas Assis Lima <lucas.lima@trinove.com.br>
2022-03-03 09:16:12 -0300

    Projeto criado

---
+++ src/main/java/br/com/trinove/TrinoveSistemasApplication.java
@@ -0,0 +1,13 @@
+package br.com.trinove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrinoveSistemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrinoveSistemasApplication.class, args);
+	}
+
+}
