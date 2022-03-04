99078e825085b419ac8857a78bbd9230206448c4
Lucas Assis Lima <lucas.lima@trinove.com.br>
2022-03-04 10:35:01 -0300

    Ajuste no endpoint /categorias/{id}

--- src/main/java/br/com/trinove/services/CategoriaService.java
+++ src/main/java/br/com/trinove/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.trinove.domain.Categoria;
 import br.com.trinove.repositories.CategoriaRepository;
+import br.com.trinove.services.exception.ObjectNotFoundException;
 
 
 @Service
@@ -15,9 +16,20 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
+/*	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: "+ id +", Tipo: " + Categoria.class.getName());
+		}
+		return obj.orElse(null);
+		}
+		
+		*/
+	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		}
 
 }
